2baee2231f8fab07e69d216bb6659ce92f35004b
John Hewitt <john.hewitt64@gmail.com>
2015-03-28 00:58:15 -0400

    respelled express

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 var graph = require('./graph');
 var io = require('socket.io');
-var express = require('expres');
-
+var app = require('express');
+var cparser = require('cookie-parser');
 app.get('/', function(req, res, err) {
 	res.sendFile(__dirname + '/index.html');
 });
